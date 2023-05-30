dcf68bcf5356489d3966e98a423d943688c2b26a
jemba narcisse <cabliveer@gmail.com>
2023-05-30 15:36:03 +0200

    Create calculator functional component

---
+++ src/components/calculator/Calculator.js
@@ -0,0 +1,28 @@
+import './calculator.css';
+
+export default function Calculator() {
+  return (
+    <div className="container">
+      <div className="item">0</div>
+      <div className="item">AC</div>
+      <div className="item">+/-</div>
+      <div className="item">%</div>
+      <div className="item">÷</div>
+      <div className="item">7</div>
+      <div className="item">8</div>
+      <div className="item">9</div>
+      <div className="item">x</div>
+      <div className="item">4</div>
+      <div className="item">5</div>
+      <div className="item">6</div>
+      <div className="item">-</div>
+      <div className="item">1</div>
+      <div className="item">2</div>
+      <div className="item">3</div>
+      <div className="item">+</div>
+      <div className="item">0</div>
+      <div className="item">.</div>
+      <div className="item">=</div>
+    </div>
+  );
+}
