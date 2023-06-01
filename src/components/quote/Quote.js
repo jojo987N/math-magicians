855d9954b02b88eff9d52dadb96cf878f4612f4c
jemba narcisse <cabliveer@gmail.com>
2023-06-01 16:26:49 +0200

    Create quote functional component

---
+++ src/components/quote/Quote.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import './quote.css';
+import URL_BASE, { API_KEY } from '../../global';
+
+export default function Quote() {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch(`${URL_BASE}=happiness`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Api-Key': API_KEY,
+        },
+      });
+      const json = await res.json().catch(() => setHasError(true));
+      setData(json);
+      setIsLoading(false);
+    })();
+  }, [setData]);
+
+  return (hasError && <div>Error! Try again</div>) || (isLoading ? <div className="loading">Loading...</div> : <blockquote>{`${data[0].quote} –${data[0].author}`}</blockquote>);
+}
