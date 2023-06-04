8bee5f05c8b01f9f99d4647ba5d1e51a6cec02e5
jemba narcisse <cabliveer@gmail.com>
2023-06-04 15:50:23 +0200

    Remove comments line in Display component

--- src/components/display/Display.js
+++ src/components/display/Display.js
@@ -1,18 +1,5 @@
-// import { useEffect } from 'react';
-
-export default function Display({ inputs, setInputs }) {
-  // console.log('ddgd', inputs.replace(/^[+\-÷x]$/, (input) => `${obj.total}${input}`));
-
-  // useEffect(() => {
-  //   setInputs(inputs.replace(/^[+\-÷x]$/, (input) => `${obj.total}${input}`));
-  // }, []);
-  // setInputs(inputs.replace(/^[+\-÷x]$/, (input) => `${obj.total}${input}`));
-  if (/(AC|=)/.test(inputs)) { setInputs(''); }
-
-  // setInputs(inputs.replace(/^[+\-÷x]$/, (input) => `0${input}`));
-  // if (/^[+\-÷x]$/.test(inputs)) { setInputs(inputs.replace(/[+\-÷x]/g,
-  // (input) => ` ${input} `)); }
+export default function Display({ inputs }) {
   return (
-    <div>{inputs.replace(/[+\-÷x]/g, (input) => ` ${input} `)}</div>
+    <div>{inputs.replace(/[+\-÷x%]/g, (input) => ` ${input} `)}</div>
   );
 }
