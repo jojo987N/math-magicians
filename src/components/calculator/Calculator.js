c4812d159739097422ec4836d6127c8117336b98
jemba narcisse <cabliveer@gmail.com>
2023-06-04 06:36:04 +0200

    Add advanced functionnality to calculator

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -1,18 +1,34 @@
 import { useState } from 'react';
 import calculate from '../../logic/calculate';
 import './calculator.css';
+import Display from '../display/Display';
 
 export default function Calculator() {
   const [obj, setObj] = useState({ total: 0 });
+  const [inputs, setInputs] = useState('');
   function handleClick(e) {
-    setObj({ ...obj, ...calculate(obj, e.target.textContent) });
+    const input = e.target.textContent;
+    console.log({ ...obj, ...calculate(obj, input) });
+    setObj({ ...obj, ...calculate(obj, input) });
+    setInputs((val) => {
+      if (/^$/.test(val) && /[+\-÷x]/.test(input)) { return `${obj.total}${input}`; }
+      const inputs = val + input;
+      return /[+\-÷x]{2}/.test(inputs.slice(-2)) ? inputs.slice(0, -2) + inputs.slice(-1) : inputs;
+    });
   }
   return (
     <div className="container">
-      <div className="item">{(obj.total && obj.next) || obj.next ? obj.next : obj.total || 0}</div>
+      <div className="item">
+        <div>
+          <Display inputs={inputs} setInputs={setInputs} obj={obj} />
+          {/* <div>{(obj.total && obj.next) || obj.next ? obj.next : obj.total || 0}</div> */}
+          {/* (\d+\.?\d*)[+\-÷x](\d+\.?\d*)[+\-÷x=] */}
+          <div>{(/(\d+\.?\d*)[+\-÷x](\d+\.?\d*)[+\-÷x=]/.test(inputs) && obj.total) || (/^$/.test(inputs) && (obj.total || 0))}</div>
+        </div>
+      </div>
       <button type="button" className="item" onClick={handleClick}>AC</button>
-      <button type="button" className="item" onClick={handleClick}>+/-</button>
-      <button type="button" className="item" onClick={handleClick}>%</button>
+      <button type="button" className="item">+/-</button>
+      <button type="button" className="item">%</button>
       <button type="button" className="item" onClick={handleClick}>÷</button>
       <button type="button" className="item" onClick={handleClick}>7</button>
       <button type="button" className="item" onClick={handleClick}>8</button>
