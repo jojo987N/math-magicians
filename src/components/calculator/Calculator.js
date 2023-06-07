d359c904b7603120ed16b3b3871fd833f86c9d9d
jemba narcisse <cabliveer@gmail.com>
2023-06-07 06:36:18 +0200

    Added text to the left of calculator

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import calculate from '../../logic/calculate';
 import './calculator.css';
+import './calculator.scss';
 
 export default function Calculator() {
   const [obj, setObj] = useState({ total: 0 });
@@ -9,26 +10,29 @@ export default function Calculator() {
   }
   return (
     <div className="container">
-      <div className="item">{(obj.total && obj.next) || obj.next ? obj.next : obj.total || 0}</div>
-      <button type="button" className="item" onClick={handleClick}>AC</button>
-      <button type="button" className="item" onClick={handleClick}>+/-</button>
-      <button type="button" className="item" onClick={handleClick}>%</button>
-      <button type="button" className="item" onClick={handleClick}>÷</button>
-      <button type="button" className="item" onClick={handleClick}>7</button>
-      <button type="button" className="item" onClick={handleClick}>8</button>
-      <button type="button" className="item" onClick={handleClick}>9</button>
-      <button type="button" className="item" onClick={handleClick}>x</button>
-      <button type="button" className="item" onClick={handleClick}>4</button>
-      <button type="button" className="item" onClick={handleClick}>5</button>
-      <button type="button" className="item" onClick={handleClick}>6</button>
-      <button type="button" className="item" onClick={handleClick}>-</button>
-      <button type="button" className="item" onClick={handleClick}>1</button>
-      <button type="button" className="item" onClick={handleClick}>2</button>
-      <button type="button" className="item" onClick={handleClick}>3</button>
-      <button type="button" className="item" onClick={handleClick}>+</button>
-      <button type="button" className="item" onClick={handleClick}>0</button>
-      <button type="button" className="item" onClick={handleClick}>.</button>
-      <button type="button" className="item" onClick={handleClick}>=</button>
+      <p>Let&apos;s do some math!</p>
+      <div className="calculator">
+        <div className="item">{(obj.total && obj.next) || obj.next ? obj.next : obj.total || 0}</div>
+        <button type="button" className="item" onClick={handleClick}>AC</button>
+        <button type="button" className="item" onClick={handleClick}>+/-</button>
+        <button type="button" className="item" onClick={handleClick}>%</button>
+        <button type="button" className="item" onClick={handleClick}>÷</button>
+        <button type="button" className="item" onClick={handleClick}>7</button>
+        <button type="button" className="item" onClick={handleClick}>8</button>
+        <button type="button" className="item" onClick={handleClick}>9</button>
+        <button type="button" className="item" onClick={handleClick}>x</button>
+        <button type="button" className="item" onClick={handleClick}>4</button>
+        <button type="button" className="item" onClick={handleClick}>5</button>
+        <button type="button" className="item" onClick={handleClick}>6</button>
+        <button type="button" className="item" onClick={handleClick}>-</button>
+        <button type="button" className="item" onClick={handleClick}>1</button>
+        <button type="button" className="item" onClick={handleClick}>2</button>
+        <button type="button" className="item" onClick={handleClick}>3</button>
+        <button type="button" className="item" onClick={handleClick}>+</button>
+        <button type="button" className="item" onClick={handleClick}>0</button>
+        <button type="button" className="item" onClick={handleClick}>.</button>
+        <button type="button" className="item" onClick={handleClick}>=</button>
+      </div>
     </div>
   );
 }
