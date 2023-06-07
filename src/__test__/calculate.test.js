680f14bb7968221dd1a7ff8346a24697c544da07
Fezzopro <fezzopro@gmail.com>
2023-06-07 17:56:40 +0200

    Check when total ≠ 0, if button name is ., next property is follow by .

--- src/__test__/calculate.test.js
+++ src/__test__/calculate.test.js
@@ -16,4 +16,22 @@ describe('Tests for calculate function ', () => {
   expect(obj).toEqual({ total: null, next: null, operation: null });
 });
 
+it('Check if button name is ".", next property is equal 0.', () => {
+  let obj = { total: 0 };
+  const buttonName = '.';
+  obj = { ...obj, ...calculate(obj, buttonName) };
+  expect(obj.next).toEqual('0.');
+});
+
+it('Check when total ≠ 0, if button name is ".", next property is follow by "."', () => {
+  let obj = { total: 0 };
+  const randomNumber = Math.floor(Math.random() * 10).toString();
+  obj = { ...obj, ...calculate(obj, randomNumber) };
+  const buttonName = '.';
+  obj = { ...obj, ...calculate(obj, buttonName) };
+  expect(obj.next).toEqual(`${randomNumber}.`);
+});
+
+
+
 })
